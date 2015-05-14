c995dcc22c1786b3e7697a9b9edf81d575a995f3
yasvisu <yasvisu@gmail.com>
2015-05-14 21:09:32 +0300

    Made initial DK project

---
+++ Dromund Kaas/Dromund Kaas - Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DromundKaas
+{
+    class SpaceShips
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<10; i++)
+            {
+                //INTRODUCTION
+            }
+
+            bool end = false;
+            while(!end)
+            {
+                //MAIN LOOP
+            }
+
+            for(int i=0; i<10; i++)
+            {
+                //CREDITS
+            }
+        }
+    }
+}
