a2bdb3cb27054f00f369064532bc59a9998abcd1
yasvisu <yasvisu@gmail.com>
2015-05-18 16:06:19 +0300

    Made Entities classes

--- Dromund Kaas/Dromund Kaas - Main/Entities.cs
+++ Dromund Kaas/Dromund Kaas - Main/Entities.cs
@@ -2,38 +2,54 @@
 
 namespace DromundKaas
 {
-    //Entity
-    public abstract class Entity : IComparable
+    /// <summary>
+    /// Point structure to hold 2D coordinates.
+    /// </summary>
+    public struct Point
     {
-        public string Name;
-        public int Power;
-        public char[,] Image;
+        public int X;
+        public int Y;
 
-        public Entity() { }
-
-        public Entity(string N, int P, char[,] I)
+        public Point(int x, int y)
         {
-            this.Name = N;
-            this.Power = P;
-            this.Image = I;
+            this.X = x;
+            this.Y = y;
         }
+    }
 
-        public int CompareTo(Object a)
+    public abstract class Entity
+    {
+        /// <summary>
+        /// Remaining life to the entity.
+        /// </summary>
+        public int Life;
+
+        /// <summary>
+        /// Location of the entity (top left corner of the sprite).
+        /// </summary>
+        public Point Location;
+
+        /// <summary>
+        /// Type of Entity.
+        /// </summary>
+        public EntityType Type;
+
+        private Entity() { }
+
+        public Entity(int L, Point Loc, EntityType T)
         {
-            if (a == null)
-                return 1;
-
-            Entity other = a as Entity;
-            if (other != null)
-                return this.Name.CompareTo(other.Name);
-            else
-                throw new ArgumentException("Object is not a Temperature");
+            this.Life = L;
+            this.Location = Loc;
+            this.Type = T;
         }
     }
 
     public class Enemy : Entity
     {
-
+        /// <summary>
+        /// Current step in the Enemy's movement path.
+        /// </summary>
+        public int Step;
     }
 
     public class Player : Entity
@@ -41,5 +57,35 @@ namespace DromundKaas
 
     }
 
-    public class EntityType { }
+    /// <summary>
+    /// Type of Entity. To be stored as unique values in the Main module.
+    /// </summary>
+    public class EntityType : IComparable
+    {
+        public string   Name;
+        public char[,]  Sprite;
+        public int      MaxLife;
+        public string   Movement;
+
+        private EntityType() { }
+
+        public EntityType(string N, char[,] S, int M, string Mov)
+        {
+            this.Name = N;
+            this.Sprite = S;
+            this.MaxLife = M;
+            this.Movement = Mov;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            EntityType other = obj as EntityType;
+            if (other != null)
+                return this.Name.CompareTo(other.Name);
+            else
+                throw new ArgumentException("Object is not an EntityType");
+        }
+    }
 }
--- Dromund Kaas/Dromund Kaas - Main/Program.cs
+++ Dromund Kaas/Dromund Kaas - Main/Program.cs
@@ -8,9 +8,9 @@ namespace DromundKaas
     {
 
         private static HashSet<EntityType> EntityTypes;
-        private static List<Entity> Entities;
-        private static Queue<bool[]> EnemyBullets;
-        private static Queue<bool[]> PlayerBullets;
+        private static List<Enemy> Enemies;
+        private static bool[,] EnemyBullets;
+        private static bool[,] PlayerBullets;
         private static int CycleCounter;
 
         private static void Init()
@@ -20,15 +20,10 @@ namespace DromundKaas
             EntityTypes = new HashSet<EntityType>();
             LoadEntityTypes(EntityTypes);
 
-            Entities = new List<Entity>();
+            Enemies = new List<Enemy>();
 
-            EnemyBullets = new Queue<bool[]>(GlobalVar.CONSOLE_HEIGHT);
-            PlayerBullets = new Queue<bool[]>(GlobalVar.CONSOLE_HEIGHT);
-            for (int i = 0; i < GlobalVar.CONSOLE_HEIGHT; i++)
-            {
-                EnemyBullets.Enqueue(new bool[GlobalVar.CONSOLE_WIDTH]);
-                EnemyBullets.Enqueue(new bool[GlobalVar.CONSOLE_WIDTH]);
-            }
+            EnemyBullets = new bool[GlobalVar.CONSOLE_HEIGHT, GlobalVar.CONSOLE_WIDTH];
+            PlayerBullets = new bool[GlobalVar.CONSOLE_HEIGHT, GlobalVar.CONSOLE_WIDTH];
 
             CycleCounter = 0;
         }
@@ -42,24 +37,110 @@ namespace DromundKaas
             IntroOutro.Intro();
             IntroOutro.PrintSomething("lo6 lo6i lo6 lo6i lo6 lo6i");
 
+            //Load Player into Entities
+
+            var PLAYER = new Player();
+
             //2. MAIN LOOP
             bool end = false;
             while (!end)
             {
+                //Increment CycleCounter
+                CycleCounter++;
+
+                //Progress Bullets
+                RollDown(EnemyBullets);
+                RollUp(PlayerBullets);
+
+                //Match Bullets
+                //SEE IF BULLETS COLLIDE
+
+
+                //Progress Entities
+                //Progress player, based on last keypress
+                for (int i = 1; i < Enemies.Count; i++)
+                {
+                    if (Enemies[i].Step > Enemies[i].Type.Movement.Length)
+                        Enemies[i].Step = 0;
+                    MoveEntity(Enemies[i], Enemies[i].Type.Movement[Enemies[i].Step]);
+                }
                 end = true;
                 // Thread.Sleep(1000);
             }
 
 
             //3. OUTRO
-            IntroOutro.Outro();
+            // IntroOutro.Outro();
 
             Console.ReadKey();
         }
-    
+
         private static void LoadEntityTypes(HashSet<EntityType> target)
         {
 
+        }
+
+        private static void RollUp(bool[,] target)
+        {
+            for (int j = 0; j < target.GetLength(1); j++)
+            {
+                target[0, j] = false;
+            }
+            for (int i = 1; i < target.GetLength(0); i++)
+            {
+                for (int j = 0; j < target.GetLength(1); j++)
+                {
+                    if (target[i, j])
+                    {
+                        target[i, j] = false;
+                        target[i - 1, j] = true;
+                    }
+                }
+            }
+        }
+
+        private static void RollDown(bool[,] target)
+        {
+            for (int j = 0; j < target.GetLength(1); j++)
+            {
+                int temp = target.GetLength(1) - 1;
+                target[temp, j] = false;
+            }
+            for (int i = 0; i < target.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < target.GetLength(1); j++)
+                {
+                    if (target[i, j])
+                    {
+                        target[i, j] = false;
+                        target[i + 1, j] = true;
+                    }
+                }
+            }
+        }
+
+        private static void MoveEntity(Entity e, char direction)
+        {
+            int x = 0, y = 0;
+            switch (direction)
+            {
+                case 'u':
+                    y = -1;
+                    break;
+                case 'd':
+                    y = 1;
+                    break;
+                case 'l':
+                    x = -1;
+                    break;
+                case 'r':
+                    x = 1;
+                    break;
+                default:
+                    break;
+            }
+            e.Location.X += x;
+            e.Location.Y += y;
         }
     }
 }
