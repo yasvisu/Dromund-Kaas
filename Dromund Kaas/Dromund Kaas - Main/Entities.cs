25dda0ad9d1c2df6da10c3e414b3caaeb0cd4951
Ivaylo Hristov <hristofff@yahoo.com>
2015-05-19 17:21:35 +0300

    added ExtractEntityData method

--- Dromund Kaas/Dromund Kaas - Main/Entities.cs
+++ Dromund Kaas/Dromund Kaas - Main/Entities.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace DromundKaas
 {
@@ -30,17 +33,23 @@ namespace DromundKaas
         public Point Location;
 
         /// <summary>
+        /// Color of the entity.
+        /// </summary>
+        public ConsoleColor Color;
+
+        /// <summary>
         /// Type of Entity.
         /// </summary>
         public EntityType Type;
 
         private Entity() { }
 
-        public Entity(int L, Point Loc, EntityType T)
+        public Entity(int L, Point Loc, EntityType T, ConsoleColor C)
         {
             this.Life = L;
             this.Location = Loc;
             this.Type = T;
+            this.Color = C;
         }
     }
 
@@ -62,10 +71,10 @@ namespace DromundKaas
     /// </summary>
     public class EntityType : IComparable
     {
-        public string   Name;
-        public char[,]  Sprite;
-        public int      MaxLife;
-        public string   Movement;
+        public string Name;
+        public char[,] Sprite;
+        public int MaxLife;
+        public string Movement;
 
         private EntityType() { }
 
@@ -86,6 +95,37 @@ namespace DromundKaas
                 return this.Name.CompareTo(other.Name);
             else
                 throw new ArgumentException("Object is not an EntityType");
+        }
+
+        static void ExtractData(HashSet<Enemy> Target)
+        {
+            string path = @"../../EntityTypes.dk";
+            string text = File.ReadAllText(path);
+            string[] types = text.Split(new string[] { ">>>>>" }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 1; i < types.Length - 1; i++)
+            {
+                string[] typeFeatures = types[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                string pattern = @"name=""([\w]+)""";
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(types[i]);
+                Console.WriteLine("Name: {0}", match.Groups[1]);
+                if (match.Groups[1].ToString() == "LukeSkywalker")
+                {
+                    Console.WriteLine("Shape: {0}", typeFeatures[2]);
+                }
+                else
+                {
+                    Console.WriteLine("Shape: {0}", typeFeatures[3]);
+                }
+                pattern = @"life=""([\d]+)""";
+                regex = new Regex(pattern);
+                match = regex.Match(types[i]);
+                Console.WriteLine("Life: {0}", match.Groups[1]);
+                pattern = @"movement=""([\w]+)""";
+                regex = new Regex(pattern);
+                match = regex.Match(types[i]);
+                Console.WriteLine("Move: {0}\n", match.Groups[1]);
+            }
         }
     }
 }
