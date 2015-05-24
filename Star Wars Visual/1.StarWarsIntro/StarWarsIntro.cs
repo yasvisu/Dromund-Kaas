f3cde47740773fe7f26415154b5bf233290284be
npaunov <nvpaunov@gmail.com>
2015-05-24 17:50:19 +0300

    Sega i v julto!

--- Star Wars Visual/1.StarWarsIntro/StarWarsIntro.cs
+++ Star Wars Visual/1.StarWarsIntro/StarWarsIntro.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
-
 class StarWarsIntro
 {
     static void Main()
     {
-
+        Console.SetBufferSize(80, 80);
+        Console.CursorVisible = false;
         System.Threading.Thread.Sleep(500);
         WriteDromundKaas();
         System.Threading.Thread.Sleep(3000);
@@ -25,14 +25,10 @@ class StarWarsIntro
             StarBeepIntro();
         };
         //TheImperialMarch();
+        StarwarsInroText();
         WriteTheConsoleGame();
         Console.Write("THE CONSOLE GAME");
         Console.WriteLine();
-
-
-
-
-
     }
 
 
@@ -62,90 +58,85 @@ class StarWarsIntro
     {
         for (int i = 0; i < 37; i = i + 9)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("THE CONSOLE GAME".PadLeft(0 + i, ' '));
             System.Threading.Thread.Sleep(380);
             for (int j = 0; j < 16; j++)
             {
                 Console.Write("\b");
             }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
     static void WriteInAGalaxy()
     {
-        Console.WriteLine(@"
-
-  ___                  ____       _                  
- |_ _|_ __     __ _   / ___| __ _| | __ ___  ___   _ 
-  | || '_ \   / _` | | |  _ / _` | |/ _` \ \/ / | | |
-  | || | | | | (_| | | |_| | (_| | | (_| |>  <| |_| |
- |___|_| |_|  \__,_|  \____|\__,_|_|\__,_/_/\_\\__, |
-                                               |___/ 
-
-  _____                _____               _                                  
- |  ___|_ _ _ __      |  ___|_ _ _ __     / \__      ____ _ _   _             
- | |_ / _` | '__|     | |_ / _` | '__|   / _ \ \ /\ / / _` | | | |            
- |  _| (_| | |     _  |  _| (_| | |     / ___ \ V  V / (_| | |_| |  _   _   _ 
- |_|  \__,_|_|    ( ) |_|  \__,_|_|    /_/   \_\_/\_/ \__,_|\__, | (_) (_) (_)
-                  |/                                        |___/             
-");
+        string text = System.IO.File.ReadAllText(@"..\..\WriteInAGalaxy.txt");
+        Console.WriteLine(text);
     }
 
     static void WritePresents()
     {
-        Console.WriteLine(@"
-         _____  _____  ______  _____ ______ _   _ _______ _____      
-         |  __ \|  __ \|  ____|/ ____|  ____| \ | |__   __/ ____|     
-         | |__) | |__) | |__  | (___ | |__  |  \| |  | | | (___       
-         |  ___/|  _  /|  __|  \___ \|  __| | . ` |  | |  \___ \      
-         | |    | | \ \| |____ ____) | |____| |\  |  | |  ____) | _ _ 
-         |_|    |_|  \_\______|_____/|______|_| \_|  |_| |_____(_|_|_)
-");
+        string text = System.IO.File.ReadAllText(@"..\..\WritePresents.txt");
+        Console.WriteLine(text);
     }
 
     static void WriteDromundKaas()
     {
-
-        Console.WriteLine(@"  
-                      _______                   
-                     |__   __|                  
-                        | | ___  __ _ _ __ ___  
-                        | |/ _ \/ _` | '_ ` _ \ 
-                        | |  __/ (_| | | | | | |
-                        |_|\___|\__,_|_| |_| |_|
-  _ _ _____                                      _   _  __              _ _ 
- ( | )  __ \                                    | | | |/ /             ( | )
-  V V| |  | |_ __ ___  _ __ ___  _   _ _ __   __| | | ' / __ _  __ _ ___V V 
-     | |  | | '__/ _ \| '_ ` _ \| | | | '_ \ / _` | |  < / _` |/ _` / __|   
-     | |__| | | | (_) | | | | | | |_| | | | | (_| | | . \ (_| | (_| \__ \   
-     |_____/|_|  \___/|_| |_| |_|\__,_|_| |_|\__,_| |_|\_\__,_|\__,_|___/   
-");
+        string text = System.IO.File.ReadAllText(@"..\..\WriteDromundKaas.txt");
+        Console.WriteLine(text);
     }
 
     static void WriteStarWars()
     {
-        Console.WriteLine(@"             .               .    .          .              .   .         .
-               _________________      ____         __________
- .       .    /                 |    /    \    .  |          \
-     .       /    ______   _____| . /      \      |    ___    |     .     .
-             \    \    |   |       /   /\   \     |   |___>   |
-           .  \    \   |   |      /   /__\   \  . |         _/               .
- .     ________>    |  |   | .   /            \   |   |\    \_______    .
-      |            /   |   |    /    ______    \  |   | \           |
-      |___________/    |___|   /____/      \____\ |___|  \__________|    .
-  .     ____    __  . _____   ____      .  __________   .  _________
-       \    \  /  \  /    /  /    \       |          \    /         |      .
-        \    \/    \/    /  /      \      |    ___    |  /    ______|  .
-         \              /  /   /\   \ .   |   |___>   |  \    \
-   .      \            /  /   /__\   \    |         _/.   \    \            +
-           \    /\    /  /            \   |   |\    \______>    |   .
-            \  /  \  /  /    ______    \  |   | \              /          .
- .       .   \/    \/  /____/      \____\ |___|  \____________/  
-                               .                                        .
-     .                           .         .               .                 .
-");
+        char[,] zvezdi = new char[30, 80];
+
+        Random randomZvezdi = new Random();
+        randomZvezdi.Next(50, 100);
+        char[][] allStars = new char[19][];
+
+        using (var textRead = new StreamReader(@"..\..\..\1.StarWarsintro\WriteStarWars.txt"))
+        {
+            for (int i = 0; i < 19; i++)
+            {
+                var line = textRead.ReadLine();
+                allStars[i] = line.ToCharArray();
+            }
+        }
+
+        for (int i = 0; i < 150; i++)
+        {
+            zvezdi[randomZvezdi.Next(0, 30), randomZvezdi.Next(0, 80)] = '.';
+        }
+
+        for (int i = 0; i < allStars.Length; i++)
+        {
+            for (int j = 0; j < allStars[i].Length; j++)
+            {
+                if (allStars[i][j] == ' ')
+                {
+                    Console.Write(zvezdi[i, j]);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(allStars[i][j]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        //string text = System.IO.File.ReadAllText(@"..\..\WriteStarWars.txt");
+        //Console.WriteLine(text);
+
+        //Console.WriteLine(@"");
         //string text = System.IO.File.ReadAllText(@"C:\Users\Nicks\Desktop\StarWarsLogo.txt");
         //Console.WriteLine("{0}", text);           // TAka se vadi ot tekstovi fail
+        // "proj/bin/Debug/blabla.exe"
+        //->
+        // "proj/x/baba"
+        //-> "./../../x/baba"
     }
     static void StarBeepIntro()
     {
@@ -179,6 +170,57 @@ class StarWarsIntro
         Console.Beep(1046, d);
         Console.Beep(987, d);
         Console.Beep(1046, d);
+    }
+    private static void StarwarsInroText()
+    {
+        Console.WriteLine();
+        Queue<string> statText = new Queue<string>();
+        statText.Enqueue("A long time ago, in a galaxy far,");
+        statText.Enqueue("far away....");
+        statText.Enqueue("  ");
+        statText.Enqueue("It is a period of serious code writing.");
+        statText.Enqueue("Rebel new programmers, striking");
+        statText.Enqueue("from a hidden base, at SoftUni");
+        statText.Enqueue("had their first victory against");
+        statText.Enqueue("the Programming Basics Exam.");
+        statText.Enqueue("  ");
+        statText.Enqueue("After the battle, rebel");
+        statText.Enqueue("codders managed to write");
+        statText.Enqueue("their entire homeworks!!!");
+        statText.Enqueue("During this intensive code writing");
+        statText.Enqueue("they were able to create");
+        statText.Enqueue("an entirely new Console Game");
+        statText.Enqueue("Which will be able to impress");
+        statText.Enqueue("the judging commissions' hearts!");
+        statText.Enqueue(" ");
+        statText.Enqueue("Pursued by the Nakov empire's");
+        statText.Enqueue("strick and powerful lectors");
+        statText.Enqueue("these brave new codders");
+        statText.Enqueue("are preparing for ");
+        statText.Enqueue("the C# Advanced Exam.");
+        statText.Enqueue("Succeeding in this task");
+        statText.Enqueue("will bring peace and freedom");
+        statText.Enqueue("to them.....");
+        statText.Enqueue(" ");
+        statText.Enqueue(" ");
+        statText.Enqueue("at least for a couple of days");
+
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        int move = 0;
+        foreach (var item in statText)
+        {
+            if (move > 28)
+            {
+                break;
+            }
+            Console.SetCursorPosition(20, 20 + move);
+            Console.WriteLine("{0}", item.PadRight(20));
+            System.Threading.Thread.Sleep(500);
+            move++;
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
     }
 }
 
